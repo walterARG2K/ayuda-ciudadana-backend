558f8a7063a5fdc0ca9763662afb54d5e4531a4b
Walter Ortiz <80477967+walterARG2K@users.noreply.github.com>
2022-12-05 19:04:43 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://ayuda-ciudadana.vercel.app/",
+            value: "https://ayuda-ciudadana.vercel.app",
           },
           {
             key: "Access-Control-Allow-Methods",
